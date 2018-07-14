78473098efcc12174a091c4e3c1780dd9d319036
萧柱杰 <xizozhujie@banggood.com>
2018-07-14 17:45:37 +0800

    login+menu

--- application/backend/controller/Backend.php
+++ application/backend/controller/Backend.php
@@ -1,9 +1,87 @@
 <?php
 namespace app\backend\controller;
 use think\Controller;
+use think\Db;
+use think\Session;
 
-
+header("Content-type: text/html; charset=utf-8");
 class Backend extends Controller
 {
-    
+	// succcess 返回
+    protected $suc = array('status'=>1, 'msg'=>'success', 'data'=>'');
+    // failed 返回
+    protected $fai = array('status'=>0, 'msg'=>'failed', 'data'=>'');
+    // true为返回sql
+    protected $fetchSql = false;
+
+
+
+    /**
+     * 后台数据编辑器，统一处理添加、编辑
+     * @author XZJ 2018-07-14T15:09:13+0800
+     * @param  [type] $table [description]
+     * @param  [type] $where [description]
+     * @param  [type] $data  [description]
+     * @return [type]        [description]
+     */
+    protected function edit($table='', $data='', $where='')
+    {
+    	if(!$table){
+    		return false;
+    	}
+    	$obj = db($table);
+    	if($where){
+    		$obj->where($where);
+    		$type = 'update';
+    	}else{
+    		$type = 'insert';
+    	}    	
+    	$res = $obj->fetchSql($this->fetchSql)->$type($data);    	
+    	return $res;
+    }
+
+    /**
+     * 后台数据查询
+     * @author XZJ 2018-07-14T17:15:43+0800
+     * @param  [type]  $table   [description]
+     * @param  string  $field   [description]
+     * @param  string  $where   [description]
+     * @param  integer $page    [description]
+     * @param  integer $limit   [description]
+     * @param  boolean $isCount [description]
+     * @return [type]           [description]
+     */
+    protected function select($table, $where='', $field='*', $page=1, $limit=50, $isCount=false)
+    {
+    	if(!$table){
+    		return false;
+    	}
+    	$obj = db($table);
+    	if($isCount){
+    		$type = 'count';
+    	}else{
+    		$type = 'select';
+    		if($page != 0){
+    			// 0则查询所有数据
+    			$obj->page("{$page}, {$limit}");
+    		}
+    		$obj->field($field);
+    	}
+
+    	$res = $obj->where($where)->fetchSql($this->fetchSql)->$type();
+    	return $res;
+    }
+
+
+    /**
+     * 后台数据count查询
+     * @author XZJ 2018-07-14T17:16:01+0800
+     * @param  [type] $table [description]
+     * @param  string $where [description]
+     * @return [type]        [description]
+     */
+    protected function count($table, $where='')
+    {
+		return db($table)->where($where)->fetchSql($this->fetchSql)->count();
+    }
 }
--- application/backend/controller/Login.php
+++ application/backend/controller/Login.php
@@ -2,19 +2,109 @@
 namespace app\backend\controller;
 use think\Controller;
 use think\Db;
+use think\Session;
 
 class Login extends Controller
 {
     public function index()
     {    
-        return $this->fetch();
+    	if(session::get('user')){
+			$this->redirect('Index/index');
+    	}else{
+        	return $this->fetch();
+    	}
+    	
     }    
 
+
+    /**
+     * ajax 登陆
+     * @author XZJ 2018-07-14T10:34:24+0800
+     * @return [type] [description]
+     */
     public function check()
-    {
-    	$res['status'] = 1;
-    	$res['msg'] = 'success';
-    	$res['data'] = input('post.');
+    {    	
+    	$res['status'] = 0;
+    	$res['msg'] = '账号或密码错误';
+
+    	$email = input('post.email');
+    	$pwd = input('post.password');
+
+    	$info = $this->getAdmin($email);
+    	if(!$info){
+    		return $res;
+    	}else{
+    		$match = $this->comparePwd($info['pwd'], $pwd);
+    		if($match){
+    			$this->login($info);
+    			$res['status'] = 1;
+    			$res['msg'] = 'success';
+    		}
+    	}    	    
     	return $res;
     }
+
+    /**
+     * 根据邮箱获取用户用户
+     * @author XZJ 2018-07-14T10:34:01+0800
+     * @param  string $email [description]
+     * @return [type]        [description]
+     */
+    private function getAdmin($email = '')
+    {
+    	if(!$email){
+    		return false;
+    	}
+    	$res = Db::table('admin')->where('email', $email)->find();
+    	return $res;
+    }
+
+    /**
+     * 对比密码
+     * @author XZJ 2018-07-14T10:33:49+0800
+     * @param  [type] $data  [description]
+     * @param  [type] $input [description]
+     * @return [type]        [description]
+     */
+    private function comparePwd($data, $input)
+    {    	
+    	$res = false;
+    	if(md5($input) == $data){
+    		$res = true;
+    	}
+    	return $res;
+    }
+
+    /**
+     * 登陆后session等信息操作
+     * @author XZJ 2018-07-14T10:33:26+0800
+     * @param  [type] $info [description]
+     * @return [type]       [description]
+     */
+    private function login($info)
+    {
+    	// session 录入
+		Session::set('user.email', $info['email']);
+		Session::set('user.name', $info['name']);
+		Session::set('user.last_login_ip', $info['last_login_ip']);
+		Session::set('user.last_login_time', $info['last_login_time']);
+
+		// 更新登陆信息
+		$update['last_login_ip'] = $_SERVER["REMOTE_ADDR"];;
+		$update['last_login_time'] = time();
+		Db::table('admin')->where('id', $info['id'])->update($update);
+    }
+
+    /**
+     * 退出登陆
+     * @author XZJ 2018-07-14T10:38:41+0800
+     * @return [type] [description]
+     */
+    public function logout()
+    {
+    	Session::delete('user');
+    	$this->redirect('Login/index');
+    }
+
+
 }
