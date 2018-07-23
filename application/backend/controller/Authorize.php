b0d617fcf90aec772b765982fa14a7577160c288
萧柱杰 <xizozhujie@banggood.com>
2018-07-23 10:50:02 +0800

    权限生效

--- application/backend/controller/Authorize.php
+++ application/backend/controller/Authorize.php
@@ -15,12 +15,12 @@ class Authorize extends Controller
     {                
         
         $res = $this->verifyAuth();
+        // var_dump($res);
         if(!$res){
             if(request()->isAjax()){
                 $fail = array('status'=>0, 'msg'=>'no permission');
-                // header("Content-Type: application/json; charset=utf-8");
-                // return $fail;die;
-                // echo json_encode($fail);die;
+                header("Content-Type: application/json; charset=utf-8");                
+                echo json_encode($fail);die;
             }else{
                 echo 'no permission';die;
             }
@@ -28,28 +28,34 @@ class Authorize extends Controller
 
     }
 
+    /**
+     * 检测用户权限
+     * @author XZJ 2018-07-23T10:48:21+0800
+     * @return [type] [description]
+     */
     private function verifyAuth()
     {        
         $admin = session::get('admin');        
         
         $con = request()->controller();
         $act = request()->action();  
+
         // 未登陆跳转
         if(!$admin && $con != 'Login'){
             $this->redirect('Login/index');die;
-        }      
+        }   
+
         // 超管权限
-        if($admin['id'] == 1){
-            return true;
-        }
-        // var_dump($con);
+        if($admin['id'] == 1){    
+            return true;        
+        }        
         // 免验证控制器
         if(in_array($con, $this->exempt_controller)){
             return true;
         }        
         // 验证具体权限        
-        // $admin_auth = $admin['auth'];        
-        $admin_auth = model('Admin')->getAdminAuth();
+        // $admin_auth = model('Admin')->getAdminAuth();
+        $admin_auth = $admin['auth'];                
         if(isset($admin_auth[$con])){
             if(!in_array($act, $admin_auth[$con])){
                 return false;
